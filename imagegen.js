f21aba8474dc61de849da7d8d34daf93f586ef7a
tmp009 <128413103+tmp009@users.noreply.github.com>
2024-01-08 13:42:50 +0200

    imagegen

---
+++ imagegen.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const fs = require('fs')
+const { OpenAI } = require('openai');
+const { exit } = require('process');
+
+
+const openai = new OpenAI();
+
+async function runimageGen(text) {
+    const data = await openai.images.generate({
+        prompt: text ,
+        model:'dall-e-3'
+    })
+
+    return data.data
+}
+
+if (process.argv.length < 3) {
+    console.log(`node imagegen.js <text>`)
+    exit(0)
+} 
+
+const text = process.argv[2];
+
+runimageGen(text).then(data => console.log(data[0].url))
