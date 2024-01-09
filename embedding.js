c78181a8642a585cef87b1548b9a292736b92c7a
tmp009 <128413103+tmp009@users.noreply.github.com>
2024-01-09 10:07:26 +0200

    compare similarity

--- embedding.js
+++ embedding.js
@@ -2,24 +2,39 @@ require('dotenv').config();
 
 const { OpenAI } = require('openai');
 const { exit } = require('process');
+const math = require('mathjs');
 
 
 const openai = new OpenAI();
 
-async function runEmbedding(text) {
+function similarity(a, b) {
+    const dotProduct = math.dot(a, b);
+    const magnitudeA = math.norm(a);
+    const magnitudeB = math.norm(b);
+    return dotProduct / (magnitudeA * magnitudeB);
+}
+
+async function getEmbedding(text) {
     const data = await openai.embeddings.create({
         input:text, model:'text-embedding-ada-002',
     })
-
     return data.data
 }
 
-if (process.argv.length < 3) {
-    console.log(`node embedding.js <text>`)
+async function runEmbedding(text1, text2) {
+    const embedding1 = await getEmbedding(text1)
+    const embedding2 = await getEmbedding(text2)
+
+    console.log(similarity(embedding1[0].embedding, embedding2[0].embedding))
+}
+
+if (process.argv.length < 4) {
+    console.log(`node embedding.js <text1> <text2>`)
     exit(0)
 } 
 
 
-const text = process.argv[2];
+const text1 = process.argv[2];
+const text2 = process.argv[3];
 
-runEmbedding(text).then(console.log)
+runEmbedding(text1, text2)
